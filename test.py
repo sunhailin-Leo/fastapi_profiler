e9ab6550df4aa9d682d9482c7bc222fd999209b8
sunhailinLeo <379978424@qq.com>
2023-01-03 15:02:22 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import os
 
-os.system("nosetests --with-coverage --cover-package fastapi_profiler --cover-package test")
+os.system(
+    "nosetests --with-coverage --cover-package fastapi_profiler --cover-package test"
+)
 os.system("flake8 --exclude build --max-line-length 89 --ignore=F401")
